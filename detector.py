30c9a9d059d2b0864cba653191255ed522e2bbaf
Cde57 <cristian.echeverry1@udea.edu.co>
2024-03-03 15:35:54 -0500

    new

--- detector.py
+++ detector.py
@@ -4,16 +4,17 @@ import numpy as np
 video_path = "C:\\Users\\caco2\\Desktop\\Sin título.mp4"
 cap = cv2.VideoCapture(video_path)
 
-# Definir las nuevas dimensiones deseadas para el video
-new_dimensions = (1000, 700)
+# Define the new desired dimensions for the video
+new_dimensions = (550, 500)
 
-# Definir la posición deseada de los parches
+# Define the desired position and size of the rectangle
 desired_position = (150, 150)
+desired_width = 50
+desired_height = 50
 
-# Inicializar variables de seguimiento
-object_detected = False
-object_crossed = False
-counter = 0
+# Initialize trace variables
+crossing_counter = 0 # to crossing_counter
+previous_center_x = 25 # To store the previous center position
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -21,63 +22,80 @@ while cap.isOpened():
     if not ret:
         break
 
-    # Redimensionar el frame a las nuevas dimensiones
+    # Resize the frame to the new dimensions
     frame = cv2.resize(frame, new_dimensions)
-    # Convertir el frame a formato HSV
+    # Convert the frame to HSV format
     imghsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Definir rango de color amarillo en formato HSV
-    amarillobajo = np.array([20, 50, 50], np.uint8)
-    amarilloalto = np.array([32, 255, 255], np.uint8)
+    # Define yellow color range in HSV format
+    yellowlow = np.array([20, 50, 50], np.uint8)
+    yellowhigh = np.array([32, 255, 255], np.uint8)
 
-    # Crear una máscara para objetos amarillos
-    mask_amarillo = cv2.inRange(imghsv, amarillobajo, amarilloalto)
+    # Create a mask for yellow objects
+    mask_yellow = cv2.inRange(imghsv, yellowlow, yellowhigh)
 
-    # Definir rango de color verde en formato HSV
-    verdebajo = np.array([36, 50, 50], np.uint8)
-    verdealto = np.array([75, 255, 255], np.uint8)
-    
-    # Crear una máscara para objetos verdes
-    mask_verde = cv2.inRange(imghsv, verdebajo, verdealto)
+    # Define green color range in HSV format
+    greenlow = np.array([36, 50, 50], np.uint8)
+    highgreen = np.array([75, 255, 255], np.uint8)
 
-    # Combinar las máscaras para detectar objetos amarillos y verdes
-    mask = cv2.bitwise_or(mask_amarillo, mask_verde)
+    # Create a mask for green objects
+    mask_green = cv2.inRange(imghsv, greenlow, highgreen)
 
-    # Encontrar contornos en la máscara combinada
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Define red color range in HSV format
+    redlow = np.array([0, 100, 100], np.uint8)
+    redhigh = np.array([40, 255, 255], np.uint8)
 
-    # Iterar a través de los contornos y verificar la posición deseada
+    # Create a mask for red objects
+    mask_red = cv2.inRange(imghsv, redlow, redhigh)
+
+    # Combine masks to detect yellow, green and red objects
+    combined_mask = cv2.bitwise_or(mask_yellow, cv2.bitwise_or(mask_green, mask_red))
+
+    # Apply the combined mask to the original frame to display the detected objects
+    masked_frame = cv2.bitwise_and(frame, frame, mask=combined_mask)
+
+    # Define the kernel for dilation
+    kernel = np.ones((5, 5), np.uint8)
+
+    # Apply dilation to the combined mask
+    dilated_mask = cv2.dilate(combined_mask, kernel, iterations=1)
+
+    # Show the frame with the dilated mask applied
+    cv2.imshow('Detected Objects with Dilated Masks', masked_frame)
+
+    # Find contours in the dilated mask
+    contours, _ = cv2.findContours(dilated_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
 
-        # Dibujar rectángulo alrededor del objeto
+        # Draw rectangle around the object
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Calcular el centro del rectángulo delimitador
         center_x = x + w // 2
         center_y = y + h // 2
 
-        if (
-            desired_position[0] < center_x < desired_position[0] + 50
-            and desired_position[1] < center_y < desired_position[1] + 50
-        ):
-            if np.any(mask_amarillo[y:y+h, x:x+w]) or np.any(mask_verde[y:y+h, x:x+w]):
-                if not object_detected:
-                    object_detected = True
-            else:
-                object_detected = False
+        # Check if the area of the contour is greater than 500
+        if cv2.contourArea(contour) > 500:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 1)
 
-    # Verificar si el objeto cruza la posición deseada
-    if object_detected and not object_crossed:
-        counter += 1
-        object_crossed = True
-    elif not object_detected:
-        object_crossed = False
-    cv2.putText(frame, f"Crossings: {counter}", (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1.5, (0, 0, 0), 2)
+            # Check if the object is completely inside the desired rectangle
+            if (
+                    desired_position[0] <= center_x <= desired_position[0] + desired_width and
+                    desired_position[1] <= center_y <= desired_position[1] + desired_height
+            ):
+                # Check if the object has crossed the line
+                if center_x is not None and previous_center_x < 400 and center_x > 460:
+                    crossing_counter += 1 # Increment the crossing counter
+
+        # Update the previous position of the center
+        previous_center_x = center_x
+
+    # Show the cross counter in the frame
+    cv2.putText(frame, f"Crossings: {crossing_counter}", (10, 30), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 0), 2)
     cv2.imshow('Detected Objects', frame)
-
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
